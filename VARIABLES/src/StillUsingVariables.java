466dbeed548a1594228e1e5d0e2360c547a639ed
VincentChan118 <ChanV8721@sduhsd.lan>
2017-12-01 10:47:32 -0800

    Finished 1 & 2
    Completed the Basic Printing and Variable assignments

---
+++ VARIABLES/src/StillUsingVariables.java
@@ -0,0 +1,14 @@
+public class StillUsingVariables {
+	
+	public static void main(String[] args)
+	{
+		int year;
+		String name;
+		
+		year = 2019;
+		name = "Vincent Chan";
+		
+		System.out.println("my name is " + year + " and I'll graduate in " + year + ".");
+	}
+
+}
