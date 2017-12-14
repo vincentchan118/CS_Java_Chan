d989d4786d5938901ba6f7b98287e9b4436e3eef
VincentChan118 <ChanV8721@sduhsd.lan>
2017-12-14 11:08:24 -0800

    Finished Part 1 of Assignment 8

--- For Loops/src/FizzBuzz.java
+++ For Loops/src/FizzBuzz.java
@@ -4,17 +4,17 @@ public class FizzBuzz
 	{
 		for (int n = 1 ; n<=100 ; n = n+1)
 		{
-			if (n%3==0)
+			if ((n%3==0) && (n%5==0))
+			{
+				System.out.println("FizzBuzz");
+			}
+			else if (n%3==0)
 			{
 				System.out.println("Fizz");
 			}
 			else if (n%5==0)
 			{
 				System.out.println("Buzz");
-			}
-			else if ((n%3==0) && (n%5==0))
-			{
-				System.out.println("FizzBuzz");
 			}
 			else
 			{
